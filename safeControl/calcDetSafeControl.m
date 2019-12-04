3eb90330f3f7f5adf126aff3a3e3f906100654fa
Katherine Shih <kshih@andrew.cmu.edu>
2019-12-04 15:03:01 -0500

    adding deterministic safe controller

---
+++ safeControl/calcDetSafeControl.m
@@ -0,0 +1,12 @@
+% Calculates safe control based on safety index and nominal controller.
+% Assumes full model knowledge.
+% Input: State q, nominal control u_nom, obstacles obst
+% Output: Safety index phi, safe control u_safe
+
+function [u_safe, phi] = calcDetSafeControl(q, u_nom, obst)
+
+end
+
+function phi = calcSafetyIndex(q, obst)
+
+end
