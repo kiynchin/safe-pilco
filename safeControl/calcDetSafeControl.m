ebbea555491ce5de75d817cd19adcc02ea7e288f
Katherine Shih <kshih@andrew.cmu.edu>
2019-12-05 12:57:44 -0500

    modified to work with rest of code

--- safeControl/calcDetSafeControl.m
+++ safeControl/calcDetSafeControl.m
@@ -1,26 +1,41 @@
 % Calculates safe control based on safety index and nominal controller.
 % Assumes full model knowledge.
-% Input: State q, nominal control u_nom, obstacles obst
+% Input: State q, nominal control u_nom, environment env
+%      q -- q.x, q.y, q.x_vel, q.y_vel, q.mass, q.radius
+%      control -- control.force, control.angle
+%      env -- env.left_wall, env_right_wall, env.top_wall, env.bottom_wall
+%             env.width, env.height, env.obstacles{}
+%      env.obstacles has .x, .y, .a, .b
 % Output: Safety index phi, safe control u_safe
 
-function [u_safe, phi] = calcDetSafeControl(q, u_nom, obst, box, lb, ub, K)
-optArgIdx = 5;
+function [controlSafe, phi] = calcDetSafeControl(q_all, control, env, lb, ub, K)
+q = [q_all.x; q_all.y; q_all.x_vel; q_all.y_vel]; mass = q_all.mass; dist = q_all.radius;
+u_nom = [control.force*cos(control.angle); control.force*sin(control.angle)]/mass;
+obst = env.obstacles; box = [env.left_wall, env.right_wall, env.bottom_wall, env.top_wall];
+
+optArgIdx = 4;
 % if we don't specify lower/upper bounds, set to inf
 if(nargin < optArgIdx)
     lb = -inf*ones(size(u_nom));
+else
+    lb = lb*ones(size(u_nom));
 end
 if(nargin < optArgIdx + 1)
     ub = inf*ones(size(u_nom));
+else
+    ub = ub*ones(size(u_nom));
 end
 % if we don't specify gain, set to default
 if(nargin < optArgIdx + 2)
     K = [-10, -3];
 end
-dist = 1;
 
 [H, f, A, b, phi] = makeQPConstraints(q, u_nom, obst, dist, box, K);
 
-u_safe = quadprog(H, f, A, b, [], [], lb, ub);
+opts = optimoptions(@quadprog, 'Display', 'off');
+u_safe = quadprog(H, f, A, b, [], [], lb, ub, [], opts);
+controlSafe.force = sqrt(u_safe(2)^2 + u_safe(1)^2)*mass;
+controlSafe.angle = atan2(u_safe(2), u_safe(1));
 
 if(1)
     v = -10:0.01:10; [x1 x2] = meshgrid(v);
@@ -30,6 +45,7 @@ if(1)
         condN(condN == 0) = NaN;
         cond = cond.*condN;
     end
+    figure(256);
     imagesc(flip(cond));
 end
 end
@@ -42,7 +58,8 @@ H = eye(length(u_nom)); f = -u_nom;
 A = []; b = []; phi = [];
 
 for ii = 1:size(obst, 1)
-    [indx, Am, bm] = calcSafetyIndexEllipse(q, obst(ii,:), dist, K);
+    obst_params = [obst{ii}.x, obst{ii}.y, obst{ii}.a, obst{ii}.b];
+    [indx, Am, bm] = calcSafetyIndexEllipse(q, obst_params, dist, K);
     phi = [phi; indx];
     A = [A; Am];
     b = [b; bm];
@@ -57,7 +74,7 @@ end
 % Output: Safety index phi; A matrix of constraint; b matrix of constraint
 function [phi, A, b] = calcSafetyIndexEllipse(q, obst, dist, K)
     x1_c = obst(1); x2_c = obst(2); a1 = obst(3); a2 = obst(4);
-    r1 = q(1) - x1_c; r2 = q(2) - x2_c;
+    r1 = q(1) - x1_c; r2 = q(2) - x2_c; dist = dist + 1;
     
     phi = (r1/a1)^2 + (r2/a2)^2 - dist;
     phi_d = 2*r1*q(3)/a1^2 + 2*r2*q(4)/a2^2;
@@ -68,6 +85,6 @@ end
 function [phi, A, b] = calcSafetyIndexBox(q, box, dist, K)
     left = box(1); right = box(2); lower = box(3); upper = box(4);
     
-    phi = [q(1) - left - dist; right - q(1) - dist; q(2) - lower - dist; upper - q(3) - dist];
+    phi = [q(1) - left - dist; right - q(1) - dist; q(2) - lower - dist; upper - q(2) - dist];
     A = [-1, 0; 1, 0; 0, -1; 0, 1]; b = -K*[phi(1), q(3); phi(2), -q(3); phi(3), q(4); phi(4), -q(4)]';
 end
